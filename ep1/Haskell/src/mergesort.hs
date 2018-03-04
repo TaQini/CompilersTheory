987071ce99a8c2a5461f438e16306a3973865aa2
TaQini <742954809@qq.com>
2018-03-04 22:25:04 +0800

    add haskell !!!!! done!

---
+++ ep1/Haskell/src/mergesort.hs
@@ -0,0 +1,22 @@
+import Control.Exception
+import Data.Time
+
+main = do a <- readFile "raw.dat"
+          begin <- getCurrentTime
+          evaluate (mergesort ((atoi (words a))))
+          end <- getCurrentTime
+          print(diffUTCTime end begin)
+
+-- atoi 
+atoi list = map toi list
+toi item = read item :: Int
+
+-- mergesort
+mergesort [] = [] 
+mergesort [a] = [a]
+mergesort a = let pulka = length a `div` 2 in merge (mergesort $ take pulka a) (mergesort $ drop pulka a) 
+
+-- merge
+merge a [] = a
+merge [] a = a
+merge (a:ra) (b:rb) = if a > b then ([b] ++ (merge (a:ra) (rb))) else ([a] ++ (merge (ra) (b:rb)))
